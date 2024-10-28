9275f1d8b9444bb0be41b5d523e3ceccecc69552
Lennox Mountain <lennoxmountain67@gmail.com>
2024-10-28 20:00:11 +0200

    added models for Page and sections

--- cms/settings.py
+++ cms/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # local apps
+    'cms_app',
+
+    # 3rd party apps
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- cms_app/models.py
+++ cms_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Page(models.Model):
+    title = models.CharField(max_field=200)
+    slug = models.SlugField(unique=True)
+    content = models.TextField()
+    is_published = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Section(models.Model):
+    name = models.CharField(max_length=100)
+    page = models.ForeignKey(Page, related_name='sections', on_delete=models.CASCADE)
+    content = models.TextField()
+    order = models.IntegerField(default=0)
+
+    class Meta:
+        order = ['order']
+
+    def __str__(self):
+        return f'{self.page.title} - {self.name}'
